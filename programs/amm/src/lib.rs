da643a40342338420e1bcd01882226e6cf948c00
Grant Codes <grntcodes@users.noreply.github.com>
2025-08-17 15:16:15 -0400

    perf: optimize CPI calls

--- programs/amm/src/lib.rs
+++ programs/amm/src/lib.rs
@@ -3,3 +3,4 @@
 // Update at 2025-10-09 12:02:11: perf: reduce gas costs by 15%
 // Update at 2025-10-01 22:21:47: docs: add deployment guide
 // Update at 2025-09-14 12:37:36: perf: reduce gas costs by 15%
+// Update at 2025-08-17 15:16:15: docs: update integration examples
