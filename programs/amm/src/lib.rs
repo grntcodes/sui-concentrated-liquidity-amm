28c7305d653076f593a87a2c67e1ab1f5f1913cd
Grant Codes <grntcodes@users.noreply.github.com>
2025-09-14 12:37:36 -0400

    feat: optimize transaction batching

--- programs/amm/src/lib.rs
+++ programs/amm/src/lib.rs
@@ -2,3 +2,4 @@
 // Update at 2025-09-23 01:59:01: security: add circuit breaker for volatile markets
 // Update at 2025-10-09 12:02:11: perf: reduce gas costs by 15%
 // Update at 2025-10-01 22:21:47: docs: add deployment guide
+// Update at 2025-09-14 12:37:36: perf: reduce gas costs by 15%
