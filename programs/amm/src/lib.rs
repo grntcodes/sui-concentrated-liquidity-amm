c36f38ab4134ff9a98e184ff07636a96d9e705ee
Grant Codes <grntcodes@users.noreply.github.com>
2025-10-20 17:50:05 -0400

    refactor: clean up pool initialization logic

--- programs/amm/src/lib.rs
+++ programs/amm/src/lib.rs
@@ -6,3 +6,4 @@
 // Update at 2025-08-17 15:16:15: docs: update integration examples
 // Update at 2025-09-11 13:09:12: refactor: clean up pool initialization logic
 // Update 2 at 2025-10-14 15:43:00
+// Update 4 at 2025-10-20 17:50:05
